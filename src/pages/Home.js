a59fa2e42d68d93207057b4591efa46e7cb6e597
Daniel Yuki <danielyukihiga1@gmail.com>
2023-09-27 15:34:31 -0300

    Expo + Tailwind Setup

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home(){
+    return(
+        <View className="items-center justify-center bg-primary">
+            <Text>Home</Text>
+        </View>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+        primary: "#0BFFB7",
+        secondary: "#F5FFC9",
+        tertiary: "#F8FEDF",
+        black2: "#212121",
+        light: "#FCFCFC",
+        gray2: "#E0E0E0",
+    },
+    extend: {},
+},
+  plugins: [],
+}
+
